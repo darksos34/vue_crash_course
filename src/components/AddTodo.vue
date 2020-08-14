e37bad0a732e0711859ee213ff5732c5492750af
Your Name <j.hamwijk34@gmail.com>
2020-08-14 11:53:59 +0200

    Update Main App

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,15 +1,34 @@
 <template>
   <div>
-    <form>
-      <input type="text" name="title" placeholder="AddTodo" />
+    <form @submit="addTodo">
+      <input type="text" v-model="title" name="title" placeholder="AddTodo" />
       <input type="submit" value="Submit" class="btn" />
     </form>
   </div>
 </template>
 
 <script>
+import uuid from "uuid";
 export default {
   name: "AddTodo",
+  data() {
+    return {
+      title: "",
+    };
+  },
+  methods: {
+    addTodo(e) {
+      e.preventDefault();
+      const newTodo = {
+        id: uuid.v4(),
+        title: this.title,
+        completed: false,
+      };
+
+      // Send up parent
+      this.$emit("add-todo", newTodo);
+    },
+  },
 };
 </script>
 
