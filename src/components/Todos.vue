96d606a18a6ce16bbcd05d8af269d738e0174be2
Your Name <j.hamwijk34@gmail.com>
2020-08-14 11:10:29 +0200

    Update Todos.vue

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="now" src="../assets/AudiLogo.png" />
   <div>
     <div v-bind:key="todo.id" v-for="todo in todos">
       <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo',
       todo.id)" />
+      <img alt="now" src="../assets/AudiLogo.png" />
     </div>
   </div>
 </template>
 
 <script>
 import TodoItem from "./TodoItem.vue";
+import Image from "../assets/AudiLogo.png";
 
 export default {
   name: "Todos",
