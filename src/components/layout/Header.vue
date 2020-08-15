134a9a36c35cc9b82b4e2d3ca4650e2709559d62
Your Name <j.hamwijk34@gmail.com>
2020-08-15 12:05:35 +0200

    Update Header.vue

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -2,6 +2,7 @@
   <header class="header">
     <h1>TodoList</h1>
     <div id="nav">
+      <p>Demo Project needs to be modifed</p>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
@@ -15,18 +16,16 @@ export default {
 </script>
 
 <style scoped>
-  .header {
-    background: #333;
-    color: #fff;
-    text-align: center;
-    padding: 10px;
-  }
+.header {
+  background: #333;
+  color: #fff;
+  text-align: center;
+  padding: 10px;
+}
 
-  .header a {
-    color: #fff;
-    padding-right: 5px;
-    text-decoration: none;
-  }
+.header a {
+  color: #fff;
+  padding-right: 5px;
+  text-decoration: none;
+}
 </style>
-
-
