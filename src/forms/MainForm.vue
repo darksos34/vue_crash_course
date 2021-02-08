718da06483ea24d535f5789fcb6407a9c32a066a
Your Name <j.hamwijk34@gmail.com>
2021-02-08 13:24:48 +0100

    Added Register form

---
+++ src/forms/MainForm.vue
@@ -0,0 +1,22 @@
+<!--
+  - /*
+  -  * Coding for fun created by Darksos34.
+  -  * Copyright (c) 2020.
+  -  * All rights reserved by Jordy Hamwijk
+  -  * Learning Computer Language.
+  -  */
+  -->
+
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: "MainForm"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,5 +2,12 @@
   <div class="about">
     <h1>About</h1>
     <p>Write your own text here</p>
+    <Register></Register>
   </div>
 </template>
+<script>
+import Register from "@/forms/Register";
+export default {
+  components: {Register}
+}
+</script>
